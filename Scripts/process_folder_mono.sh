1b9a260626ce544448d9a92f59fcec264df3c0c4
Henrique <henribarbosa.o@gmail.com>
2023-07-08 14:49:56 -0300

    sáb 08 jul 2023 14:49:56 -03

---
+++ Scripts/process_folder_mono.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+while read line; do
+
+	#IFS=' '
+	#read -a list <<< ${line}
+	#folder=${list[0]}
+	#lower=${list[1]}
+	#upper=${list[2]}
+	
+	echo \"${line}/image_*.tif\"
+
+	#rm thresholds.txt
+	#cp thresholds_base.txt thresholds.txt
+	#sed -i "s/A1A/${lower}/" thresholds.txt
+	#sed -i "s/A2A/${upper}/" thresholds.txt
+
+	./lerImagem -m process -r all -t 0 -p "${line}/image_*.tif"
+	
+	cp -r Files/ ${line}
+
+done < ToDoMonodisperse.txt
+
