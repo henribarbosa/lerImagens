09931f170b570e20cacb1ee7982564116badf6e5
Henrique <henribarbosa.o@gmail.com>
2023-07-08 16:58:05 -0300

    sáb 08 jul 2023 16:58:05 -03

--- include/math_functions.h
+++ include/math_functions.h
@@ -3,6 +3,8 @@
 #include <cmath>
 #include <vector>
 
+// functions for ordering vectors
+
 template <typename T> void merge(int* array, int const left, int const mid, int const right, std::vector<T>& vector);
 
 template <typename T> void mergeSort(int* array, int const begin, int const end, std::vector<T>& vector);
